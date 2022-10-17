b59bef7aba69f0503b71e93150a7084ffea01c77
blackmitnick <mkakarianderson68@gmail.com>
2022-10-17 04:42:03 +0300

    feat: Set variables

--- MAC_address_changer/mac_address.py
+++ MAC_address_changer/mac_address.py
@@ -2,9 +2,10 @@
 
 import subprocess
 
-interface = "wlan0"
-new_mac = "00:D4:K1:X9:5o:6B"
+interface = input("Enter interface >> ")
+new_mac = input("Enter new MAC >> ")
 print(f"[+] Changing MAC address for {interface} to {new_mac}")
-# subprocess.call("sudo ifconfig eth1 down", shell=True)
-# subprocess.call("sudo ifconfig eth1 hw ether 10:20:30:40:50:80", shell=True)
-# subprocess.call("sudo ifconfig eth1 up", shell=True)
+
+subprocess.call(f"sudo ifconfig {interface} down", shell=True)
+subprocess.call(f"sudo ifconfig {interface} hw ether {new_mac}", shell=True)
+subprocess.call(f"sudo ifconfig {interface} up", shell=True)
