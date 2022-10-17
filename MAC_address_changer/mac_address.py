ff7572cd1162cd25128ad1219d1b0641ac97cc25
blackmitnick <mkakarianderson68@gmail.com>
2022-10-17 04:15:51 +0300

    feat: Print change

--- MAC_address_changer/mac_address.py
+++ MAC_address_changer/mac_address.py
@@ -2,6 +2,9 @@
 
 import subprocess
 
-subprocess.call("sudo ifconfig eth1 down", shell=True)
-subprocess.call("sudo ifconfig eth1 hw ether 10:20:30:40:50:80", shell=True)
-subprocess.call("sudo ifconfig eth1 up", shell=True)
+interface = "wlan0"
+new_mac = "00:D4:K1:X9:5o:6B"
+print(f"[+] Changing MAC address for {interface} to {new_mac}")
+# subprocess.call("sudo ifconfig eth1 down", shell=True)
+# subprocess.call("sudo ifconfig eth1 hw ether 10:20:30:40:50:80", shell=True)
+# subprocess.call("sudo ifconfig eth1 up", shell=True)
