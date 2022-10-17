6a20de02e4589ac7ecded38b16e7f5f096b9fbc3
blackmitnick <mkakarianderson68@gmail.com>
2022-10-17 05:32:00 +0300

    feat: Add option parser

--- MAC_address_changer/mac_address.py
+++ MAC_address_changer/mac_address.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
 import subprocess
+from optparse import OptionParser
+
+parser = OptionParser()
+parser.add_option("-i", "--interface", dest="interface", help="interface to change it's MAC address")
+parser.parse_args()
 
 interface = input("Enter interface >> ")
 new_mac = input("Enter new MAC >> ")
 print(f"[+] Changing MAC address for {interface} to {new_mac}")
+
 
 subprocess.call(["sudo", "ifconfig", interface, "down"])
 subprocess.call(["sudo", "ifconfig", interface, "hw", "ether", new_mac])
