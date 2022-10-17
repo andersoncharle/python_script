6d262b91e1730c0cb25cdcb76aa3adf0ab73356c
blackmitnick <mkakarianderson68@gmail.com>
2022-10-17 06:32:39 +0300

    feat: Add received arguments()

--- MAC_address_changer/mac_address.py
+++ MAC_address_changer/mac_address.py
@@ -4,6 +4,13 @@ import subprocess
 from optparse import OptionParser
 
 
+def get_arguments():
+    parser = OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="interface to change it's MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
+    return parser.parse_args()
+
+
 def change_mac(interface, new_mac):
     print(f"[+] Changing MAC address for {interface} to {new_mac}")
 
@@ -12,10 +19,5 @@ def change_mac(interface, new_mac):
     subprocess.call(["sudo", "ifconfig", interface, "up"])
 
 
-parser = OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="interface to change it's MAC address")
-parser.add_option("-m", "--mac", dest="new_mac", help="new MAC address")
-
-(options, arguments) = parser.parse_args()
-
+(options, arguments) = get_arguments()
 change_mac(options.interface, options.new_mac)
